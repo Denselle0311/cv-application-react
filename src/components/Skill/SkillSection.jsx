9a31b69136c6534005c53742747f88e9d3566bd5
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-09-01 20:22:51 +0800

    add: Done skill section

---
+++ src/components/Skill/SkillSection.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import InputGroup from "../InputGroup";
+
+export default function SkillSection() {
+  const [allSKills, setAllSkills] = useState([]);
+  const [skill, setSkill] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
+
+  function clearInputs() {
+    setSelectedId(null);
+    setSkill("");
+  }
+
+  return (
+    <>
+      <span className="flex items-end">
+        <InputGroup
+          //maxSize={`${isEditing ? "max-w-72" : ""}`}
+          minSize={`${isEditing ? "min-w-72" : ""}`}
+          label="Skill"
+          type="text"
+          placeholder="Enter a Skill"
+          value={skill}
+          onChange={(e) => setSkill(e.target.value)}
+        />
+        {isEditing ? (
+          <span className="flex max-w gap-2">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setIsEditing((prev) => !prev);
+                setAllSkills((prev) => [
+                  ...prev,
+                  {
+                    id: selectedId,
+                    skill: skill,
+                  },
+                ]);
+                clearInputs();
+              }}
+              className="text-white bg-gray-500 p-1"
+            >
+              save
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setIsEditing((prev) => !prev);
+                setAllSkills((prev) => [
+                  ...prev.filter((s) => s.id != selectedId),
+                ]);
+                clearInputs();
+              }}
+              className="text-white bg-red-500 p-1"
+            >
+              del
+            </button>
+          </span>
+        ) : (
+          <button
+            className="text-white"
+            onClick={(e) => {
+              e.preventDefault();
+              setAllSkills((prev) => [
+                ...prev,
+                {
+                  id: generateId(prev.length),
+                  skill: skill,
+                },
+              ]);
+              clearInputs();
+            }}
+          >
+            +
+          </button>
+        )}
+      </span>
+      <ul className="flex gap-2 flex-wrap max-w-sm p-2">
+        {allSKills.map((e) => (
+          <li
+            onClick={() => {
+              setSkill(e.skill);
+              setIsEditing((prev) => !prev);
+              setSelectedId(e.id);
+              setAllSkills((prev) => [...prev.filter((s) => s.id != e.id)]);
+            }}
+            className="border border-black p-1 rounded-md cursor-pointer hover:bg-black hover:text-white"
+            key={e.id}
+          >
+            {e.skill}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function generateId(len) {
+  return crypto.randomUUID() + len;
+}
